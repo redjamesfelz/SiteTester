bf8b6ead1c01a36afa344fdb0f6817aad990bb1e
Red James <44889501+redjamesfelz@users.noreply.github.com>
2020-04-22 10:40:13 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<center>
+<style>
+body {
+  background-image: url('https://site-tester.planetred.world/shibememe.jpg');
+}
+</style>
+<body>
+<h1 style="color:lime;" ><font size="100" face="courier new"><br> DOMAIN TESTER </font></h1>
+
+<form action="https://site-tester.planetred.world/action_page.php" method="post">
+<p style="color:lime;"><font size="50" face="courier new"> TEST MY DOMAIN </font></p>
+
+<input type="text" name="name"><input type="submit">
+</form>
+
+
+
+</body>
+</center>
+</html>
