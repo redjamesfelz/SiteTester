1389050bb509abe134e2b2384b694a1143751e58
Red James <44889501+redjamesfelz@users.noreply.github.com>
2020-04-24 16:07:12 +1000

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,15 +2,19 @@
 <center>
 <style>
 body {
-  background-image: url('https://site-tester.planetred.world/wallpaper.jpg');
+  background-image: url('https://site-tester.planetred.world/background.png');
 }
 </style>
+
 <body>
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> YOUR SCORE IS $SITE_SCORE <br>
-<?php echo $_POST["name"]; ?><br></font></h1>
+<h1 style="color:#000080;" ><font size="100" face="verdana"> YOUR SCORE IS $SITE_SCORE <br>
+<?php 
+$url = $_POST["name"]; 
+echo $url;
+?>
+<br></font></h1>
 
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> VIRUS TOTAL </font></h1>
-
+<h1 style="color:#000080;" ><font size="100" face="verdana"><br> VIRUS TOTAL </font></h1>
 <!–– writes to domains.txt -->
 <?php
 $myfile = fopen("domains.txt", "w") or die("Unable to open file!");
@@ -20,7 +24,7 @@ fclose($myfile);
 ?>
 
 <!–– python checks domains.txt and scan in VT-->
-<p style="color:red;" ><font face="courier new">
+<p style="color:red;" ><font face="verdana">
 <?php 
 $command = escapeshellcmd('python2.7 /home/planetred1/public_html/site-tester/virustotal.py');
 $output = shell_exec($command);
@@ -28,59 +32,51 @@ echo $output;
 ?>
 </font></p>
 
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> DNS LOOKUP </font></h1>
-
-<!–– insert python to do domain whois lookup -->
+<h1 style="color:#000080;" ><font size="100" face="verdana"><br> DNS LOOKUP </font></h1>
+<!–– python to do domain whois lookup -->
 <p style="color:red;" ><font face="courier new">
 <?php 
-$command = escapeshellcmd('python2.7 /home/planetred1/public_html/site-tester/domaindns.py');
+$command = escapeshellcmd('python2.7 /home/planetred1/public_html/site-tester/domaindns.py' . $url);
 $output = shell_exec($command);
 echo $output;
 ?>
 </font></p>
 
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> DOMAIN WHOIS LOOKUP </font></h1>
+<h1 style="color:#000080;" ><font size="100" face="verdana"><br> DOMAIN WHOIS LOOKUP </font></h1>
+<!–– bash to do domain whois lookup -->
+<p style="color:red;" ><font face="verdana">
+<?php
+$output = shell_exec('whois' . $url);
+echo "<pre>$output</pre>";
+?>
+</font></p>
 
-<!–– insert python to do domain whois lookup -->
+<h1 style="color:#000080;" ><font size="100" face="verdana"><br> IP WHOIS LOOKUP </font></h1>
+<!–– bash to do ip whois lookup -->
+<p style="color:red;" ><font face="verdana">
+<?php
+$output = shell_exec('whois' . $url);
+echo "<pre>$output</pre>";
+?>
+</font></p>
 
+<h1 style="color:#000080;" ><font size="100" face="verdana"><br> SITE SCREENSHOT </font></h1>
+<!–– python to do take screenshot of site -->
+<?php 
+$command = escapeshellcmd('python2.7 /home/planetred1/public_html/site-tester/sitecapture.py');
+$output = shell_exec($command);
+echo $output;
+?>
 
-
-
-
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> IP WHOIS LOOKUP </font></h1>
-
-<!–– insert python to do ip whois lookup -->
-
-
-
-
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> SITE SCREENSHOT </font></h1>
-
-<!–– insert python to do take screenshot of site -->
-
-
-
-
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> DIRBUSTER </font></h1>
-
-<!–– insert python to do dirbuster -->
-
-
-
-
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> SUBLIST3R </font></h1>
-
-<!–– insert python to do sublister -->
-
-
-
-
-<h1 style="color:lime;" ><font size="100" face="courier new"><br> RECON </font></h1>
-
-<!–– insert python to do google search-->
-
-
-
+<h1 style="color:#000080;" ><font size="100" face="verdana"><br> SUBLIST3R </font></h1>
+<!–– python to do sublister -->
+<p style="color:red;" ><font face="courier new">
+<?php 
+$command = escapeshellcmd('python sublist3r.py -d' . $url);
+$output = shell_exec($command);
+echo $output;
+?>
+</font></p>
 
 </body>
 </center>
