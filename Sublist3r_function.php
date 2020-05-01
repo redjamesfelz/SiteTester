c943ed40e6b5a3ea6f0504312aeb9554b285ce5f
Red James <44889501+redjamesfelz@users.noreply.github.com>
2020-05-01 11:39:54 +1000

    Delete Sublist3r_function.php

--- Sublist3r_function.php
+++
@@ -1,7 +0,0 @@
-##Instal sublist3r on server first https://github.com/aboul3la/Sublist3r
-
-<?php 
-$command = escapeshellcmd('python sublist3r.py -d' . $url);
-$output = shell_exec($command);
-echo $output;
-?>
